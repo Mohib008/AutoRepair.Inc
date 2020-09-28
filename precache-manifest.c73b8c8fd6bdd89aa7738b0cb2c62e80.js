92a16659a8aa091bd4d34b505a01751ad858b0e4
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-09-28 15:56:36 -0400

    Updates

---
+++ precache-manifest.c73b8c8fd6bdd89aa7738b0cb2c62e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eaee2a3b44871d469e93bc3f284bbf",
+    "url": "/AutoRepair.Inc/index.html"
+  },
+  {
+    "revision": "bae4de64c00cc90c7ae2",
+    "url": "/AutoRepair.Inc/static/css/main.99be5b8a.chunk.css"
+  },
+  {
+    "revision": "81bcbc5d4863b22f77f0",
+    "url": "/AutoRepair.Inc/static/js/2.73e00a59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AutoRepair.Inc/static/js/2.73e00a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae4de64c00cc90c7ae2",
+    "url": "/AutoRepair.Inc/static/js/main.608bd1b5.chunk.js"
+  },
+  {
+    "revision": "279be22389aa8000e8ba",
+    "url": "/AutoRepair.Inc/static/js/runtime-main.762dd88c.js"
+  }
+]);
